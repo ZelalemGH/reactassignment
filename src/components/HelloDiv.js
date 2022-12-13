3173969a6683170edd109bd1381e8eca9c277877
zelalem Habtegiorgis <zeegiorgis@gmail.com>
2022-12-13 09:25:59 -0500

    my second commit

--- src/components/HelloDiv.js
+++ src/components/HelloDiv.js
@@ -1,5 +1,9 @@
 import React from "react";
+// Using the code in the [HelloDiv.js](Unsolved/src/components/HelloDiv.js) file as a starting point, update the `HelloDiv` function so that it returns the following JSX:
 
+//   * A heading tag introducing yourself, e.g., `<h1>Hello! My name is <insert name here></h1>`.
+
+//   * An unordered list containing at least 3 things you like (these can be anything).
 const HelloDiv = () => {
     const name = "Zelalem"
   return (
