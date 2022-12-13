7508ad4be7b4894354c3b5dd94718f50e9fc11da
zelalem Habtegiorgis <zeegiorgis@gmail.com>
2022-12-13 09:11:01 -0500

    my first commit

---
+++ src/components/HelloDiv.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const HelloDiv = () => {
+    const name = "Zelalem"
+  return (
+    <div>
+      <h1>Hello! My name is {name}</h1>
+      <ul>
+        <ol>This is react crash course</ol>
+        <ol>MERN stands for MongoDB, Express, React, Node </ol>
+        <ol>REACT is a javaScript library</ol>
+      </ul>
+    </div>
+  );
+};
+
+export default HelloDiv;
